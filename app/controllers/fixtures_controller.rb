779ce6f8960f2de8b31e24b265f46d126242e0ac
Mark Price <markempower94@gmail.com>
2021-03-02 15:11:23 +0000

    added a fixtures controller and views for index/show

---
+++ app/controllers/fixtures_controller.rb
@@ -0,0 +1,17 @@
+class FixturesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+  before_action :set_fixture, only: [:show]
+
+  def index
+    @fixtures = Fixture.all 
+  end
+  
+  def show
+  end
+  
+  private
+
+  def set_fixture
+    @fixture = Fixture.find(params[:id])
+  end
+end
