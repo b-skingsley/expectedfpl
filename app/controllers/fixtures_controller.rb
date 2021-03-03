4fd7d4efd7aca0aa96ab76c3dff0c1e6b45b03a1
Mark Price <markempower94@gmail.com>
2021-03-03 11:32:04 +0000

    inserted the fixtures logic into the module

--- app/controllers/fixtures_controller.rb
+++ app/controllers/fixtures_controller.rb
@@ -1,5 +1,3 @@
-require_relative "../helpers/fixtures.rb"
-
 class FixturesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
   # before_action :set_fixture, only: [:show]
