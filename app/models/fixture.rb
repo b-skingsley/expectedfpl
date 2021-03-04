8bfc218e68eed75e813bacea59e116dec2a54fa6
Mark Price <markempower94@gmail.com>
2021-03-04 11:55:41 +0000

    added team colors to application.scss

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -3,6 +3,17 @@ class Fixture < ApplicationRecord
   belongs_to :away_team, :class_name => "Club", :foreign_key => :away_team_id
   has_many :odds
 
+  # PG Search
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :home_team, :away_team ],
+    associated_against: {
+      club: [ :name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+
   # validates :kickoff, :gameweek, presence: true
   validates :gameweek, numericality: { greater_than: 0, less_than: 39 }
   validates :h_score, :a_score, numericality: { greater_than_or_equal_to: 0 }
