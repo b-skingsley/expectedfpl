c8437e8cb5acf4ea4b88dbcb0441bbef788559fc
ben <benjaminskingsley@gmail.com>
2021-03-04 09:38:54 +0000

    scraper files play around

---
+++ app/helpers/team_scraper_helper.rb
@@ -0,0 +1,18 @@
+require "nokogiri"
+require "open-uri"
+require 'capybara/rails'
+
+module TeamScraperHelper
+  def teamscrape
+    url = "https://fantasy.premierleague.com/entry/4921261/event/26/"
+    visit(url)
+    doc = Nokogiri::HTML.parse(body)
+    puts doc
+    # url = "https://fantasy.premierleague.com/entry/38281/event/26/"
+    # html = URI.open(url).read
+    # doc = Nokogiri::HTML(html)
+    # doc.search("div[class='PitchElementData__ElementName-sc-1u4y6pr-0 eMnDEV']").each do |data|
+    #   p data.text.strip.split
+    # end
+  end
+end
