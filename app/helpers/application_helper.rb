2d870bdf1917491a35a7e681856060ad67245144
ben <benjaminskingsley@gmail.com>
2021-03-11 23:08:18 +0000

    added Strf time to dates in player view, and fixed other small changes mentioned by Alex

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -136,10 +136,10 @@ module ApplicationHelper
     fixtures_details = []
     fixtures.each do |fixture|
       if fixture.home_team == club
-        fixtures_details << {opponent: fixture.away_team.short_name, kickoff: fixture.kickoff, shirt: fixture.away_team.short_name.downcase, home_or_away: "(H)"}
+        fixtures_details << {opponent: fixture.away_team.short_name, kickoff: fixture.kickoff, shirt: fixture.away_team.short_name.downcase, home_or_away: "(H)", opponent_long: fixture.away_team.name}
       end
       if fixture.away_team == club
-        fixtures_details << {opponent: fixture.home_team.short_name, kickoff: fixture.kickoff, shirt: fixture.home_team.short_name.downcase, home_or_away: "(A)"}
+        fixtures_details << {opponent: fixture.home_team.short_name, kickoff: fixture.kickoff, shirt: fixture.home_team.short_name.downcase, home_or_away: "(A)", opponent_long: fixture.home_team.name}
       end
     end
     return fixtures_details.first(num)
